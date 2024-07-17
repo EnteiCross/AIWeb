78d6626145f3502159d3457f665b03f304d9aa3e
Brandon Jaimes <b1jaimes89@gmail.com>
2024-07-17 10:10:13 -0600

    base lista apps

--- FrontIA/src/app/modules/aplicaciones/interfaces/aplicaciones.interfaces.ts
+++ FrontIA/src/app/modules/aplicaciones/interfaces/aplicaciones.interfaces.ts
@@ -1,5 +1,5 @@
 export interface Aplication {
-    id:   string;
+    id:   number;
     name: string;
     status: string;
 }
--- FrontIA/src/app/modules/aplicaciones/pages/list-apps-page/list-apps-page.component.ts
+++ FrontIA/src/app/modules/aplicaciones/pages/list-apps-page/list-apps-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Aplication } from '../../interfaces/aplicaciones.interfaces';
+import { TitleListComponent } from '@modules/shared/components/title-list/title-list.component';
 
 @Component({
   selector: 'app-list-apps-page',
   standalone: true,
-  imports: [],
+  imports: [TitleListComponent],
   templateUrl: './list-apps-page.component.html',
   styleUrl: './list-apps-page.component.css'
 })
