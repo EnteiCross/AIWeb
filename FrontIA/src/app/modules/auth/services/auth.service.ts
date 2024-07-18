c03d0eddb57c69f46df1b15e0e44c4470d5d7316
IlseChan <86861461+IlseChan@users.noreply.github.com>
2024-07-18 13:40:36 -0600

    se añadio el registro y localstorage

--- FrontIA/src/app/modules/auth/services/auth.service.ts
+++ FrontIA/src/app/modules/auth/services/auth.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  register(usernumber: string, username: string, password: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const users = JSON.parse(localStorage.getItem('users') || '[]');
+
+      if (users.find((user: any) => user.usernumber === usernumber)) {
+        reject(new Error('El número de empleado ya está registrado'));
+      } else {
+        users.push({ usernumber, username, password });
+        localStorage.setItem('users', JSON.stringify(users));
+        resolve();
+      }
+    });
+  }
+
+  getUsers(): { usernumber: string, username: string, password: string }[] {
+    return JSON.parse(localStorage.getItem('users') || '[]');
+  }
 }
